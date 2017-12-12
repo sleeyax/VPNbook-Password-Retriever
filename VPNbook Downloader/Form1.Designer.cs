1f36e18d99e46a5cc90f2d2166d03afd87249dfd
Sleeyax <yourd3veloper@gmail.com>
2017-12-12 23:59:33 +0100

    Removed about btn

--- VPNbook Downloader/Form1.Designer.cs
+++ VPNbook Downloader/Form1.Designer.cs
@@ -39,7 +39,6 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -83,7 +82,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(83, 262);
+            this.button1.Location = new System.Drawing.Point(83, 241);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 34);
             this.button1.TabIndex = 4;
@@ -97,7 +96,7 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 151);
+            this.groupBox1.Location = new System.Drawing.Point(12, 130);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(260, 100);
             this.groupBox1.TabIndex = 5;
@@ -111,7 +110,7 @@
             this.groupBox2.Controls.Add(this.textBox3);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.ForeColor = System.Drawing.Color.Black;
-            this.groupBox2.Location = new System.Drawing.Point(12, 33);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(260, 100);
             this.groupBox2.TabIndex = 6;
@@ -148,23 +147,12 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Proxy:";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(218, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(69, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "about";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(290, 307);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(290, 288);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
@@ -194,7 +182,6 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
     }
 }
 
